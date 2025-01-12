69a33a4e48e99c29ed424ed3cbb2b742c88897f1
Adamhanafi22 <adamhanafi1922@gmail.com>
2025-01-12 22:49:11 +0700

    membuat data class

--- app/src/main/java/com/example/praktikum9/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/praktikum9/ui/viewmodel/InsertViewModel.kt
@@ -2,7 +2,15 @@ package com.example.praktikum9.ui.viewmodel
 
 import com.example.praktikum9.model.Mahasiswa
 
-
+// Event input form mahasiswa
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val gender: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
 
 // Konversi dari MahasiswaEvent ke data model Mahasiswa
 fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
