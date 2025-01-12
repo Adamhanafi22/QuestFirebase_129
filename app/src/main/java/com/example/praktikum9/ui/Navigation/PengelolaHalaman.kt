daeb52d6e5e23b214144e225c22a58ed583810e3
Adamhanafi22 <adamhanafi1922@gmail.com>
2025-01-12 23:40:16 +0700

    membuat Pengelola halaman

---
+++ app/src/main/java/com/example/praktikum9/ui/Navigation/PengelolaHalaman.kt
@@ -0,0 +1,38 @@
+package com.example.praktikum9.ui.Navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum9.ui.view.HomeScreen
+import com.example.praktikum9.ui.view.InsertMhsView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
